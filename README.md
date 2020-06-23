4ba02e62da5217d1b428b7e489e708b14b9204bf
Richard Rosario <tech2js@gmail.com>
2020-06-23 14:45:54 +0800

    added readme file and reconfigure keys

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a profile and portfolio app created in ReactJS.
+
+Feel free to clone it or download it and use it as you see fit!
--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,9 @@
-if(process.env.NODE_ENV === 'production') {
-    module.exports = require('./keys_prod');
-} else {
-    module.exports = require('./keys_dev');
-}
-// module.exports = {
-//     mongoURI: 'mongodb://Richard:13uka5M0@ds247430.mlab.com:47430/devjunc',
-//     secretOrKey: 'secret'
+// if (process.env.NODE_ENV === "production") {
+//   module.exports = require("./keys_prod");
+// } else {
+//   module.exports = require("./keys_dev");
 // }
+module.exports = {
+  mongoURI: "mongodb://Richard:13uka5M0@ds247430.mlab.com:47430/devjunc",
+  secretOrKey: "secret",
+};
